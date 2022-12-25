311216eb1ca1bfad95c38f788bbd41bd61ebd275
MikaSRahwono <mikasuryof@outlook.com>
2022-12-25 18:31:06 +0700

    Finished Navbar and Home Page

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
 import '../styles/globals.css'
+import Layout from '../components/layouts/main'
+import { ChakraProvider } from '@chakra-ui/react'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps, router }) {
+  return (
+    <ChakraProvider>
+      <Layout router={router}>
+      <Component {...pageProps} key={router.route} />
+      </Layout>
+    </ChakraProvider>
+  )
 }
